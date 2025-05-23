e7afd573494a4fb3529e1fbf41c84fe4990bc69a
Jorge Alberto García Jardon <117221593+jorge5278@users.noreply.github.com>
2025-05-23 16:05:31 -0600

    Inventario

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan'; 
+import morgan from 'morgan';
+
 import userRoutes from './src/routes/userRoutes.js';
-import setupSwagger from './src/config/swaggerConfig.js';
 import twoFARoutes from './src/routes/twoFARoutes.js';
 import crudr from './src/routes/crudr.js';
+import inventarioRoutes from './src/routes/inventarioRoutes.js';    
+import setupSwagger from './src/config/swaggerConfig.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', twoFARoutes);
 
-// Configurar Morgan 
+// Logging
 app.use(morgan('dev'));
 
-// Configurar Swagger
-setupSwagger(app);
+// 2FA/Auth
+app.use('/api/auth', twoFARoutes);
 
+// Healthcheck
 app.get('/health', (_req, res) => {
   res.status(200).json({ status: 'ok', message: 'API funcionando correctamente' });
 });
 
-app.use('/api', crudr);
+// Swagger UI
+setupSwagger(app);
 
-// Luego las rutas de usuarios
+// Rutas específicas
+app.use('/api/inventario', inventarioRoutes);  
+app.use('/api', crudr);
 app.use('/api', userRoutes);
 
-
-// Port y terminal Host
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
